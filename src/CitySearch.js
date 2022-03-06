13a5ad86b566965ec7c9bed0e107950baacd6b12
MattE55 <89932886+MattE55@users.noreply.github.com>
2022-03-06 15:18:05 -0800

    unit testing

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -17,6 +17,12 @@ class CitySearch extends Component {
     });
   };
 
+  handleItemClicked = (suggestion) => {
+    this.setState({
+      query: suggestion
+    });
+  };
+
   render() {
     return (
       <div className="CitySearch">
@@ -27,10 +33,13 @@ class CitySearch extends Component {
           onChange={this.handleInputChanged}
         />
         <ul className="suggestions">
-          {this.state.suggestions.map((suggestion) => (
-            <li key={suggestion}>{suggestion}</li>
-          ))}
-          <li key='all'>
+           {this.state.suggestions.map((suggestion) => (
+             <li
+               key={suggestion}
+               onClick={() => this.handleItemClicked(suggestion)}
+             >{suggestion}</li>
+           ))}
+          <li>
             <b>See all cities</b>
           </li>
         </ul>
