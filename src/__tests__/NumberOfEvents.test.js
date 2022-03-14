fbd6f38823e12637f795781c112e733647917590
MattE55 <89932886+MattE55@users.noreply.github.com>
2022-03-13 18:31:08 -0700

    4.3 fix

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -5,7 +5,7 @@ import NumberOfEvents from '../NumberOfEvents';
 describe("<NumberOfEvents /> component", () => {
   let NumberOfEventsWrapper;
   beforeAll(() => {
-    NumberOfEventsWrapper = shallow(<NumberOfEvents />);
+    NumberOfEventsWrapper = shallow(<NumberOfEvents numberOfEvents={32} />);
   });
 
   test('render text input', () => {
